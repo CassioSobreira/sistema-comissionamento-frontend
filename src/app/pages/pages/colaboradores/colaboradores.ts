82bb019cb7b424402dfd1c971563722d4e509dcb
CassioGuimaraes <131621517+CassioSobreira@users.noreply.github.com>
2025-10-02 12:00:25 -0300

    Menu-Bar adicinado

--- src/app/pages/pages/colaboradores/colaboradores.ts
+++ src/app/pages/pages/colaboradores/colaboradores.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common'; 
 import { ButtonModal } from './components/button-modal/button-modal';
 import { TableFilterBasicDemo } from './components/table/table';
+import { MenuBar } from '../../../components/shared-components/components/menu/menu-bar/menu-bar';  
+
 @Component({
   selector: 'app-colaboradores',
   standalone: true,
   imports: [
     CommonModule,
     ButtonModal,
-    TableFilterBasicDemo
-    
+    TableFilterBasicDemo,
+    MenuBar
   ],
   templateUrl: './colaboradores.html',
   styleUrls: ['./colaboradores.css']
