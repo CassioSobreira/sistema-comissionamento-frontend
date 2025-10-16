c547361ce3b0ab0d1d1d33892cbc500f0e1f17ba
Lucas Braga <130006732+LucasBraga2@users.noreply.github.com>
2025-10-16 15:36:13 -0300

    Criacao do componente Compartilhado configuracoes do usuario

---
+++ src/app/components/shared-components/components/config-user/config-user.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { MenuBar} from '../menu/menu-bar/menu-bar';
+@Component({
+  selector: 'app-config-user',
+  imports: [MenuBar],
+  templateUrl: './config-user.html',
+  styleUrl: './config-user.css'
+})
+export class ConfigUser {
+
+}
