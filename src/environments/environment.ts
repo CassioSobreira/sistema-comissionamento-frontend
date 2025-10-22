39db0ce2cfd55ecfb6ab5ffb0138fdf9970aecf6
Lucas Braga <130006732+LucasBraga2@users.noreply.github.com>
2025-10-22 15:53:48 -0300

    Rota backend no vercel comentada

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://projeto-comissionamento-backend.vercel.app/api' 
+  apiUrl: //'https://projeto-comissionamento-backend.vercel.app/api' 
+  'http://localhost:3000/api'
 };
