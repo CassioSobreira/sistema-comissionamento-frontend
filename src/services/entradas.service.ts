f579faf870383a49f10b2a2256dacb914f8afb11
Lucas Braga <130006732+LucasBraga2@users.noreply.github.com>
2025-11-10 07:41:24 -0300

    Chamada de novas requisicoes

--- src/services/entradas.service.ts
+++ src/services/entradas.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';  import { environment } from '../environments/environment';
+import { Observable } from 'rxjs';  
+import { environment } from '../environments/environment';
   export interface Entrada{
     id_entrada: number;
     nome_entrada: string;
-
+    campo_json: any;
+    template_html: string;
   }
   @Injectable({
     providedIn: 'root'
@@ -28,5 +30,18 @@ import { Observable } from 'rxjs';  import { environment } from '../environments
       return this.http.get<Entrada[]>(url);
   }
 
+  /**
+   * Busca os detalhes completos de uma entrada (template) específica pelo ID.
+   * Esta função é a que o seu DocumentoCreateComponent usará.
+   * Chama a rota: GET /api/entradas/:id
+   * @param id_entrada O ID da entrada (template) a ser buscada.
+   */
+  buscarEntradaPorId(id_entrada: number): Observable<Entrada> {
+    // Constrói a URL final, ex: /api/entradas/1
+    const url = `${this.apiUrl}/${id_entrada}`;
+    
+    // Faz a requisição GET e espera um objeto do tipo 'Entrada'
+    return this.http.get<Entrada>(url);
+  }
   
   }
