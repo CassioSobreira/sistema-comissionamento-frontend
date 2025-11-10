187b9f2898fd4e9c64f4cec9ab55dbf662d84063
Lucas Braga <130006732+LucasBraga2@users.noreply.github.com>
2025-11-10 07:39:16 -0300

    Criacao do forms de entrada

--- src/app/pages/pages/admin-dashboard/forms/entrada-forms/entrada-forms.ts
+++ src/app/pages/pages/admin-dashboard/forms/entrada-forms/entrada-forms.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { tap, Observable, forkJoin, of } from 'rxjs'; // Import forkJoin and of
+import { switchMap, finalize } from 'rxjs/operators';
+
+// Imports do PrimeNG
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
+import { ButtonModule } from 'primeng/button';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+
+import { DynamicDialogRef, DynamicDialogConfig } from 'primeng/dynamicdialog';
+import { MessageService } from 'primeng/api';
+import { MultiSelectModule } from 'primeng/multiselect'; 
+
+import { EntradasService, Entrada } from '../../../../../../services/entradas.service';
 
 @Component({
   selector: 'app-entrada-forms',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    SelectModule,
+    ButtonModule,
+    ToggleSwitchModule,
+    MultiSelectModule,
+    ToggleButtonModule
+  ],
   templateUrl: './entrada-forms.html',
   styleUrl: './entrada-forms.css'
 })
