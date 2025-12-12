70d965ae7b62496c8ac38ceae537fe863a11f3e1
Lucas Braga <130006732+LucasBraga2@users.noreply.github.com>
2025-12-12 14:08:44 -0300

    dominio Custom

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,12 @@ const hostname = window.location.hostname;
 // se tiver "vercel.app" na URL → estamos no Vercel
 const isLoacal = hostname.includes('localhost');
 const isVercel = hostname.includes('vercel.app');
+const isDomainCustom = hostname.includes('cassiovinicius.dev'); 
 export const environment = {
   production: false, // isso aqui não faz diferença pra API agora
   apiUrl: isLoacal
     ? 'http://localhost:3000/api' // API LOCAL
-    : isVercel
+    : isVercel || isDomainCustom
       ? 'https://projeto-comissionamento-backend-pg.vercel.app/api' // API VERCEL (Frontend)
       : 'http://172.25.0.189:3000/api'// API VERCEL (VM / máquina)
 };
