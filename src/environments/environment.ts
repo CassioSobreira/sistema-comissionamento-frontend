e5624ccb12bd19a284d6dbdb4f8ba64200cd85e7
Lucas Braga <130006732+LucasBraga2@users.noreply.github.com>
2025-12-12 12:48:21 -0300

    URL do backend na VM

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,14 @@ const hostname = window.location.hostname;
 
 // se tiver "vercel.app" na URL → estamos no Vercel
 const isLoacal = hostname.includes('localhost');
-
+const isVercel = hostname.includes('vercel.app');
 export const environment = {
   production: false, // isso aqui não faz diferença pra API agora
   apiUrl: isLoacal
     ? 'http://localhost:3000/api' // API LOCAL
-    : 'https://projeto-comissionamento-backend-pg.vercel.app/api'// API VERCEL (VM / máquina)
+    : isVercel
+      ? 'https://projeto-comissionamento-backend-pg.vercel.app/api' // API VERCEL (Frontend)
+      : 'http://172.25.0.189:3000'// API VERCEL (VM / máquina)
 };
 
 console.log('Hostname:', hostname);
